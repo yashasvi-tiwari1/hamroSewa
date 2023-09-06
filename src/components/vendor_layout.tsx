e5f739e76e00258cf5ddf2315d41716b96728f43
yashasvi <yashasvi.tiwari.4648@gmail.com>
2023-09-06 20:16:53 +0545

    Vendor dashboard Design

---
+++ src/components/vendor_layout.tsx
@@ -0,0 +1,19 @@
+
+import { ReactNode } from "react";
+import Navbar from "@sewa/components/vendor_navbar";
+import Sidebar from "@sewa/components/vendor_sidebar";
+
+interface LayoutProps {
+    children: ReactNode;
+}
+export default function Layout({ children }: LayoutProps) {
+    return (
+        <>
+            <Navbar />
+            <main className="min-h-screen bg-gray-200 flex">
+                <Sidebar />
+                <div className="flex-grow pl-60 pr-8 pt-6">{children}</div>
+            </main>
+        </>
+    );
+}
---
+++ src/pages/vendor/index.tsx
@@ -0,0 +1,55 @@
+import Layout from "../../components/vendor_layout";
+import { ReactElement } from "react";
+import { NextPageWithLayout } from "@sewa/pages/_app";
+import {
+    IconActivity,
+    IconCurrencyDollar, IconCurrencyRupeeNepalese,
+    IconSquareLetterB,
+
+} from "@tabler/icons-react";
+
+const Vendor_Dashboard: NextPageWithLayout = () => {
+    return (
+        <>
+            <div className="">
+                <div className="w-[980px] bg-dashboard p-6 rounded-xl">
+                    <span className="text-xl font-semibold "> Summary </span>
+                    <div className="flex justify-between mt-10 ">
+                        <div className="bg-orange-200 p-6 w-52 text-center rounded-lg">
+                            <div className="flex justify-center">
+                                <IconCurrencyRupeeNepalese className="w-9 h-9 text-orange-600 " />
+                            </div>
+                            <div className="font-semibold text-lg">3</div>
+                            <div className="font-semibold text-lg text-gray-600">
+                                Total Earning
+                            </div>
+                        </div>
+                        <div className="bg-purple-200 p-6 rounded-lg text-center w-52 ">
+                            <div className="flex justify-center">
+                                <IconSquareLetterB className="w-9 h-9 text-purple-600" />
+                            </div>
+                            <div className="font-semibold text-lg">100</div>
+                            <div className="font-semibold text-lg text-gray-600">
+                               Total  Booking
+                            </div>
+                        </div>
+                        <div className="bg-red-200 p-6 rounded-lg text-center w-52 ">
+                            <div className="flex justify-center">
+                                <IconActivity className="w-9 h-9 text-red-600" />
+                            </div>
+                            <div className="font-semibold text-lg">100</div>
+                            <div className="font-semibold text-lg text-gray-600">
+                                Active Booking
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+Vendor_Dashboard.getLayout = function getLayout(page: ReactElement) {
+    return <Layout>{page}</Layout>;
+};
+export default Vendor_Dashboard;
